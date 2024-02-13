46792855b0dfd0ec088028a26f6c01b693ccbe11
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-02-14 02:43:22 +0300

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,7 +5,7 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
     Return:
@@ -26,23 +26,20 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized_route() -> str:
-    """GET /api/v1/unauthorized
-    Return
-      - Raises 401 error
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - Aborts
     """
     abort(401)
 
 
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden_route() -> str:
-    """GET /api/v1/forbidden
-    Return
-      - Raises 403 error
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/unauthorized
+    Return:
+      - Aborts
     """
     abort(403)
-
-
-if __name__ == '__main__':
-    pass
