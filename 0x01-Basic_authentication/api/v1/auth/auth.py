93e188977c82f9b496ac0a9378f8871eb684f13f
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-02-14 13:56:05 +0300

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,66 +1,24 @@
 #!/usr/bin/env python3
-"""
-Route module for the API
-"""
-from os import getenv
-from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+"""Authentication"""
+
+from flask import request
+from typing import List, TypeVar
 
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
+class Auth:
+    """Auth class"""
 
-if getenv("AUTH_TYPE") == "auth":
-    auth = Auth()
-elif getenv("AUTH_TYPE") == "basic_auth":
-    auth = BasicAuth()
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require auth
+        """
+        return False
 
+    def authorization_header(self, request=None) -> str:
+        """Authorization header
+        """
+        return None
 
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """
-    Unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """
-    Forbidden handler.
-    """
-    return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def before_request():
-    """
-    handler before_request
-    """
-    authorized_list = ['/api/v1/status',
-                       '/api/v1/unauthorized/', '/api/v1/forbidden']
-
-    if auth and auth.require_auth(request.path, authorized_list):
-        if not auth.authorization_header(request):
-            abort(401)
-        if not auth.current_user(request):
-            abort(403)
-
-
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user
+        """
+        return None
