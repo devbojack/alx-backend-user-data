19a680882b10efd5b26b46224818fd9de97fd6a1
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-02-15 23:06:00 +0300

    Et moi et moi et moi

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -36,6 +36,14 @@ def view_one_user(user_id: str = None) -> str:
     return jsonify(user.to_json())
 
 
+@app_views.route("/users/me", methods=['GET'], strict_slashes=False)
+def get_authenticated_user():
+    """Get authenticated user"""
+    if request.current_user is None:
+        abort(404)
+    return jsonify(request.current_user.to_json())
+
+
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
     """ DELETE /api/v1/users/:id
