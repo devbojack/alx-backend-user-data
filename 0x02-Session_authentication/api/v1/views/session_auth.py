1137c272e9518c28b730142f2fc573a4f365ec89
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-02-16 21:27:53 +0300

    Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -40,16 +40,16 @@ def session_login() -> str:
     return user_dict
 
 
-# @app_views.route(
-#     '/auth_session/logout',
-#     methods=['DELETE'],
-#     strict_slashes=False)
-# def session_logout() -> str:
-#     """ DELETE /api/v1/auth/session/logout
-#     Returns deleted json (if correctly done)
-#     404 if fails
-#     """
-#     from api.v1.app import auth
-#     if auth.destroy_session(request):
-#         return jsonify({}), 200
-#     abort(404)
+@app_views.route(
+    '/auth_session/logout',
+    methods=['DELETE'],
+    strict_slashes=False)
+def session_logout() -> str:
+    """ DELETE /api/v1/auth/session/logout
+    Returns deleted json (if correctly done)
+    404 if fails
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request):
+        return jsonify({}), 200
+    abort(404)
