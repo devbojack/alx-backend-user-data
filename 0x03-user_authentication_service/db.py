b5fe9a2a19256f3f7782657017d945990109d675
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-02-21 21:00:30 +0300

    Create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,12 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-from user import Base, User
-from typing import TypeVar
 
-VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+from user import Base, User
 
 
 class DB:
@@ -35,8 +31,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Adds a new user to the Database.
+        """Add a new user to the database
         """
         if not email or not hashed_password:
             return
@@ -45,27 +40,3 @@ class DB:
         session.add(user)
         session.commit()
         return user
-
-    def find_user_by(self, **kwargs) -> User:
-        """
-        Finds a User in the Database.
-        """
-        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
-            raise InvalidRequestError
-        session = self._session
-        try:
-            return session.query(User).filter_by(**kwargs).one()
-        except Exception:
-            raise NoResultFound
-
-    def update_user(self, user_id: int, **kwargs) -> None:
-        """
-        updating a user in the database
-        """
-        session = self._session
-        user = self.find_user_by(id=user_id)
-        for k, v in kwargs.items():
-            if k not in VALID_FIELDS:
-                raise ValueError
-            setattr(user, k, v)
-        session.commit()
