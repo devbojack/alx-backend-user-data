57ded4d79f99fb03b211aa73d19a441e4acb5ef4
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-02-21 20:11:08 +0300

    Create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add user to db
+        """Add a new user to the database
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
