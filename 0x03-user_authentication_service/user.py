f1c983d9eae383c720ca65417b2b23fa239020e2
dev.bojack <98508704+devbojack@users.noreply.github.com>
2024-02-23 01:59:21 +0300

    User Model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 """User Model"""
-from sqlalchemy import Column, Integer, String
+
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
 class User(Base):
-    """User class"""
-    __tablename__ = "users"
+    """
+    User class
+    """
+
+    __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
